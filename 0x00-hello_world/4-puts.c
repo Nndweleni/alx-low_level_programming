5c629db3f2fc9d2b8dc65768b8eb74ef8100e6c0
Nndweleni <nndweleni.work@gmail.com>
2023-03-16 08:46:25 -0700

    Hellow puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Entry point - Program that will display a line using the PUTS function
+ *
+ * Return: 0
+ * 
+ **/
+
+int main(void) { 
+	puts("Programming is like building a multilingual puzzle\n");	
+	Return (0);
+}
