eaee32791e6affc3d556294e6970a957d4b3ae9c
Nndweleni <nndweleni.work@gmail.com>
2023-03-16 08:46:46 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Entry point - This program will display a line using the printf fucntion.
+ *
+ * Return: 0
+ *
+ */	
+
+int main (void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
