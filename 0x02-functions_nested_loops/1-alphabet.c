4e2d7a447b9d3c65eb202f645db7846dfc60da70
Nndweleni <nndweleni.work@gmail.com>
2023-03-20 05:49:47 -0700

    The alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,8 @@
 /**
  * print_alphabet - Printing alphabets
  *
+ * main - Printing the code above
+ *
  * Return: 0
  */
 void print_alphabet(void)
@@ -11,4 +13,9 @@ void print_alphabet(void)
 	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 		_putchar(alphabets);
 		_putchar("\n");
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
